4dcdae0e3bddef2db678e193ada0c23d53182d01
Rostislav Brož <scraptechguy@gmail.com>
2022-09-28 09:58:23 +0200

    Initial Commit

---
+++ Rotty/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rotty
+//
+//  Created by Rostislav Brož on 9/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rotty/RottyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RottyApp.swift
+//  Rotty
+//
+//  Created by Rostislav Brož on 9/28/22.
+//
+
+import SwiftUI
+
+@main
+struct RottyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

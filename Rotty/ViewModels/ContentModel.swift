cbb874b8a5cb2d6418c675fc2c5e564d7fc7e69e
Rostislav Brož <scraptechguy@gmail.com>
2022-09-28 10:16:36 +0200

    Create basic file structure

---
+++ Rotty/ViewModels/ContentModel.swift
@@ -0,0 +1,8 @@
+//
+//  ContentModel.swift
+//  Rotty
+//
+//  Created by Rostislav Brož on 9/28/22.
+//
+
+import Foundation
---
+++ Rotty/Views/Onboarding/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingView.swift
+//  Rotty
+//
+//  Created by Rostislav Brož on 9/28/22.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}

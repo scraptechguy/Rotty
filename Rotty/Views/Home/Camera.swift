9f3b125b834518d70d73aaebff16e0210f2a77b1
Rostislav Brož <scraptechguy@gmail.com>
2022-09-30 07:28:37 +0200

    Create a file for camera code

---
+++ Rotty/Views/Home/Camera.swift
@@ -0,0 +1,20 @@
+//
+//  Camera.swift
+//  Rotty
+//
+//  Created by Rostislav Brož on 9/29/22.
+//
+
+import SwiftUI
+
+struct Camera: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Camera_Previews: PreviewProvider {
+    static var previews: some View {
+        Camera()
+    }
+}

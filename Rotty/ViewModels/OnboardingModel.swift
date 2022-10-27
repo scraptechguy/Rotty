aa45e98dd5ce73a41528783e58003c78135fce1a
Rostislav Brož <scraptechguy@gmail.com>
2022-10-27 08:51:42 +0200

    Create `OnboardingModel`

---
+++ Rotty/ViewModels/OnboardingModel.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingModel.swift
+//  Rotty
+//
+//  Created by Rostislav Brož on 10/27/22.
+//
+
+import SwiftUI
+
+struct OnboardingModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingModel_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingModel()
+    }
+}

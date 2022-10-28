5288b50280d87ea3903846b13ca46907ea3bb9db
Rostislav Brož <scraptechguy@gmail.com>
2022-10-28 15:57:48 +0200

    Create `HelpView`

---
+++ Rotty/Views/Home/HelpView.swift
@@ -0,0 +1,20 @@
+//
+//  HelpView.swift
+//  Rotty
+//
+//  Created by Rostislav Brož on 10/28/22.
+//
+
+import SwiftUI
+
+struct HelpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HelpView_Previews: PreviewProvider {
+    static var previews: some View {
+        HelpView()
+    }
+}

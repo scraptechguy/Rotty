40ecaf76a08bde8350f094914d9f8e753cc08a21
Rostislav Brož <scraptechguy@gmail.com>
2022-10-29 09:05:30 +0200

    Create missions view

---
+++ Rotty/Views/Home/MissionsView.swift
@@ -0,0 +1,20 @@
+//
+//  MissionsView.swift
+//  Rotty
+//
+//  Created by Rostislav Brož on 10/29/22.
+//
+
+import SwiftUI
+
+struct MissionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MissionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        MissionsView()
+    }
+}

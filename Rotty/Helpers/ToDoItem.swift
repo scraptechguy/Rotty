8a353aa9bcbb4cf78a78bf125f46780924f147df
Rostislav Brož <scraptechguy@gmail.com>
2022-12-05 17:13:34 +0100

    Create ToDoItem.swift

---
+++ Rotty/Helpers/ToDoItem.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoItem.swift
+//  Rotty
+//
+//  Created by Rostislav Brož on 12/5/22.
+//
+
+import SwiftUI
+
+struct ToDoItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ToDoItem_Previews: PreviewProvider {
+    static var previews: some View {
+        ToDoItem()
+    }
+}

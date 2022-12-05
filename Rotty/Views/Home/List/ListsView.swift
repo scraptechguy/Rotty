dceb9e3fe722a20273e79b614851a7f6dae614c0
Rostislav Brož <scraptechguy@gmail.com>
2022-12-05 17:38:06 +0100

    Implement initial to do item design in lists view

--- Rotty/Views/Home/List/ListsView.swift
+++ Rotty/Views/Home/List/ListsView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct ListsView: View {
+    
+    @State var shoppingList: [String] = ["Kinder maxi king", "Kinder bueno", "Tiger", "Bagetka sunka syr", "Kysele rybicky"]
+    @State var shoppingListDone: [Bool] = Array(repeating: false, count: 5)
+    @State var completed: [String] = ["Rohliky", "Mliko (tvuj otec se s nim vratil)", "Maslo", "Paprika", "Rajcata", "Panev", "Syr", "Sunka"]
+    @State var completedDone: [Bool] = Array(repeating: true, count: 8)
+    
     var body: some View {
         ZStack {
             Color("Background")
@@ -20,11 +26,23 @@ struct ListsView: View {
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .padding(.leading)
                 
+                VStack {
+                    ForEach(shoppingList.indices, id: \.self) { i in
+                        ToDoItem(name: shoppingList[i], done: shoppingListDone[i])
+                    }
+                }.padding(.bottom)
+                
                 Text("Completed")
                     .foregroundColor(Color("Font"))
                     .font(.title.bold())
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .padding(.leading)
+                
+                VStack {
+                    ForEach(completed.indices, id: \.self) { i in
+                        ToDoItem(name: completed[i], done: completedDone[i])
+                    }
+                }.padding(.bottom)
             }
         }
     }

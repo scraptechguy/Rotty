0b952132533d0978c11e492df601523bc080588f
Rostislav Brož <scraptechguy@gmail.com>
2023-01-02 13:49:29 +0100

    Create TimeFormatting.swift

---
+++ Rotty/Helpers/TimeFormatting.swift
@@ -0,0 +1,30 @@
+//
+//  TimeFormatting.swift
+//  Rotty
+//
+//  Created by Rostislav Brož on 1/2/23.
+//
+
+import Foundation
+
+func calculateTimeSince(date: Date) -> String {
+    
+    let minutes = Int(-date.timeIntervalSinceNow) / 60
+    let hours = minutes / 60
+    let days = hours / 24
+    
+    if minutes < 120 {
+        
+        return "\(minutes) minutes ago"
+        
+    } else if minutes >= 120 && hours < 48 {
+        
+        return "\(hours) hours ago"
+        
+    } else {
+        
+        return "\(days) days ago"
+        
+    }
+    
+}

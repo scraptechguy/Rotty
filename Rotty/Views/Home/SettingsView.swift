fa426f0e0297b6fe4a63e9c4ccea492bde7785a1
Rostislav Brož <scraptechguy@gmail.com>
2023-01-03 16:19:06 +0100

    Create SettingsView.swift

---
+++ Rotty/Views/Home/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  Rotty
+//
+//  Created by Rostislav Brož on 1/3/23.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}

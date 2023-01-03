66f197f1fe9247a922eee3993e6922c4f68a7814
Rostislav Brož <scraptechguy@gmail.com>
2023-01-03 16:19:27 +0100

    Create LearnView.swift

---
+++ Rotty/Views/Home/LearnView.swift
@@ -0,0 +1,20 @@
+//
+//  LearnView.swift
+//  Rotty
+//
+//  Created by Rostislav Brož on 1/3/23.
+//
+
+import SwiftUI
+
+struct LearnView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LearnView_Previews: PreviewProvider {
+    static var previews: some View {
+        LearnView()
+    }
+}

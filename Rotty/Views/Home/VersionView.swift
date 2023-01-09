f156f19abf685d28b6e0a1ff39b120970723067b
Rostislav Brož <scraptechguy@gmail.com>
2023-01-09 09:36:16 +0100

    Create VersionView.swift

---
+++ Rotty/Views/Home/VersionView.swift
@@ -0,0 +1,20 @@
+//
+//  VersionView.swift
+//  Rotty
+//
+//  Created by Rostislav Brož on 1/9/23.
+//
+
+import SwiftUI
+
+struct VersionView: View {
+    var body: some View {
+    }
+}
+
+struct VersionView_Previews: PreviewProvider {
+    static var previews: some View {
+        VersionView()
+            .environmentObject(ContentModel())
+    }
+}

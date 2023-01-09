4782764f01ac968d3161fa506107b9d8f005e26e
Rostislav Brož <scraptechguy@gmail.com>
2023-01-09 09:36:19 +0100

    Update VersionView.swift

--- Rotty/Views/Home/VersionView.swift
+++ Rotty/Views/Home/VersionView.swift
@@ -8,7 +8,38 @@
 import SwiftUI
 
 struct VersionView: View {
+    
+    @EnvironmentObject var model: ContentModel
+    
     var body: some View {
+        ZStack {
+            VStack {
+                Spacer()
+                
+                Image("RottyAppIconSVG")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .mask(RoundedRectangle(cornerRadius: 20))
+                    .shadow(radius: 50)
+                
+                Spacer()
+                
+                Text("Version 1.0.0")
+                    .fontWeight(model.fontIsBold ? .bold : .regular)
+                    .foregroundColor(model.isHighContrast ? .primary : .secondary)
+                
+                Text("Build: 5")
+                    .font(.footnote)
+                    .fontWeight(model.fontIsBold ? .bold : .regular)
+                    .foregroundColor(model.isHighContrast ? .primary : .secondary)
+                    .padding(.bottom)
+                
+                Text("Copyright 2023 Rostislav Brož")
+                    .font(.footnote)
+                    .fontWeight(model.fontIsBold ? .bold : .regular)
+                    .foregroundColor(model.isHighContrast ? .primary : .secondary)
+            }
+        }
     }
 }
 

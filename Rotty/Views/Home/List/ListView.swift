ca0dd09ae4972db2cc929d07613bd0fcb2ad6710
Rostislav Brož <scraptechguy@gmail.com>
2023-01-10 08:14:35 +0100

    Create ListView.swift

---
+++ Rotty/Views/Home/List/ListView.swift
@@ -0,0 +1,20 @@
+//
+//  ListView.swift
+//  Rotty
+//
+//  Created by Rostislav Brož on 1/10/23.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}

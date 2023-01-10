40cdc738373113317ec3b71ca97e014f659da0f6
Rostislav Brož <scraptechguy@gmail.com>
2023-01-10 08:13:42 +0100

    Create AddListView.swift

---
+++ Rotty/Views/Home/List/AddListView.swift
@@ -0,0 +1,20 @@
+//
+//  AddListView.swift
+//  Rotty
+//
+//  Created by Rostislav Brož on 1/10/23.
+//
+
+import SwiftUI
+
+struct AddListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddListView()
+    }
+}

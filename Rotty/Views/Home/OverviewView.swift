7be1e4c9e432fb656871bc5e6024c19bf899acef
Rostislav Brož <scraptechguy@gmail.com>
2023-01-11 07:14:49 +0100

    Add some charts

--- Rotty/Views/Home/OverviewView.swift
+++ Rotty/Views/Home/OverviewView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Charts
 
 struct OverviewView: View {
     
@@ -15,6 +16,9 @@ struct OverviewView: View {
     @State var showViews: [Bool] = Array(repeating: false, count: 8)
     @State var isInEnglish = true
     @State var index = 0
+    @State var currentTab: String = "7 days"
+    @State var currentActiveItem: SiteView?
+    @State var plotWidth: CGFloat = 0
     
     let myMissionsHeading: LocalizedStringKey = "myMissionsHeading"
     let level1: LocalizedStringKey = "level1"
@@ -94,7 +98,7 @@ struct OverviewView: View {
                             Button(action: {
                                 
                             }, label: {
-                                Text(level6)
+                                Text("Missionary")
                                     .font(.headline)
                                     .fontWeight(model.fontIsBold ? .bold : .regular)
                             })
@@ -106,10 +110,129 @@ struct OverviewView: View {
                     }.opacity(showViews[0] ? 1 : 0)
                         .offset(y: showViews[0] ? 0 : 200)
                 }
+                
+                // MARK: - Charts
+                
+                VStack(alignment: .leading, spacing: 12) {
+                    HStack {
+                        Text("Food saved")
+                            .fontWeight(.semibold)
+                        
+                        Spacer()
+                        
+                        Picker("", selection: $currentTab) {
+                            Text("Overall")
+                                .tag("Overall")
+                            
+                            Text("30 days")
+                                .tag("30 days")
+                            
+                            Text("7 days")
+                                .tag("7 days")
+                        }.pickerStyle(.segmented)
+                    }.padding(.bottom)
+                    
+                    Chart {
+                        ForEach(sample_analytics) { item in
+                            LineMark(
+                                x: .value("Hour", item.hour, unit: .hour),
+                                y: .value("Views", item.views)
+                            ).interpolationMethod(.catmullRom)
+                            
+                            AreaMark(
+                                x: .value("Hour", item.hour, unit: .hour),
+                                y: .value("Views", item.views)
+                            ).interpolationMethod(.catmullRom)
+                                .foregroundStyle(Color("AccentColor").opacity(0.1).gradient)
+                            
+                            if let currentActiveItem, currentActiveItem.id == item.id {
+                                
+                                RuleMark(x: .value("Hour", currentActiveItem.hour))
+                                    .lineStyle(.init(lineWidth: 2, miterLimit: 2, dash: [2], dashPhase: 5))
+                                    .offset(x: (plotWidth / CGFloat(sample_analytics.count)) / 2)
+                                    .annotation(position: .top) {
+                                        VStack(alignment: .leading, spacing: 6) {
+                                            Text("Food saved")
+                                                .font(.caption)
+                                                .foregroundColor(.gray)
+                                            
+                                            Text(String(currentActiveItem.views))
+                                                .font(.title3)
+                                        }.padding(.horizontal, 10)
+                                            .padding(.vertical, 4)
+                                            .background {
+                                                RoundedRectangle(cornerRadius: 6, style: .continuous)
+                                                    .fill(.white.shadow(.drop(radius: 2)))
+                                            }
+                                    }
+                                
+                            }
+                        }
+                    }.frame(height: 150)
+                        .chartYScale(domain: 0...15000)
+                        .chartOverlay(content: { proxy in
+                            Rectangle()
+                                .fill(.clear).contentShape(Rectangle())
+                                .gesture(
+                                    DragGesture()
+                                        .onChanged { value in
+                                            let location = value.location
+                                            
+                                            if let date: Date = proxy.value(atX: location.x) {
+                                                
+                                                let calendar = Calendar.current
+                                                let hour = calendar.component(.hour, from: date)
+                                                
+                                                if let currentItem = sample_analytics.first(where:  { item in
+                                                    calendar.component(.hour, from: item.hour) == hour }) {
+                                                    
+                                                    currentActiveItem = currentItem
+                                                    plotWidth = proxy.plotAreaSize.width
+                                                    
+                                                }
+                                            }
+                                        }
+                                        .onEnded { value in
+                                            currentActiveItem = nil
+                                        }
+                                )
+                        })
+                }.padding([.horizontal], 40)
+                    .padding(.top)
+                    .opacity(showViews[1] ? 1 : 0)
+                    .offset(y: showViews[1] ? 0 : 200)
+                
             }.onAppear(perform: animateViews)
                 .onAppear(perform: model.checkLevel)
                 .onDisappear(perform: resetViews)
+                .onAppear {
+                    for (index, _) in sample_analytics.enumerated() {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + Double(index) * 0.05) {
+                            withAnimation(.easeInOut(duration: 0.8)) {
+                                sample_analytics[index].animate = true
+                            }
+                        }
+                    }
+                }
         }.preferredColorScheme(model.isDarkMode ? .dark : .light)
+            .refreshable {
+                withAnimation {
+                    showViews[0] = false
+                    showViews[1] = false
+                    showViews[2] = false
+                    showViews[3] = false
+                    showViews[4] = false
+                    showViews[5] = false
+                    showViews[6] = false
+                    showViews[7] = false
+                }
+                
+                model.checkLevel()
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    animateViews()
+                }
+            }
     }
     
     // MARK: - animateViews()
@@ -163,6 +286,40 @@ struct OverviewView: View {
         
     }
 }
+
+// MARK: - Sample data
+
+struct SiteView: Identifiable {
+    
+    var id = UUID().uuidString
+    var hour: Date
+    var views: Double
+    var animate: Bool = false
+    
+}
+
+extension Date {
+    
+    func updateHour(value: Int) -> Date {
+        
+        let calendar = Calendar.current
+        return calendar.date(bySettingHour: value, minute: 0, second: 0, of: self) ?? .now
+        
+    }
+    
+}
+
+var sample_analytics: [SiteView] = [
+
+    SiteView(hour: Date().updateHour(value: 8), views: 1500),
+    SiteView(hour: Date().updateHour(value: 9), views: 2600),
+    SiteView(hour: Date().updateHour(value: 10), views: 3500),
+    SiteView(hour: Date().updateHour(value: 11), views: 6500),
+    SiteView(hour: Date().updateHour(value: 12), views: 3500),
+    SiteView(hour: Date().updateHour(value: 13), views: 2450),
+    SiteView(hour: Date().updateHour(value: 14), views: 7500)
+    
+]
 
 struct OverviewView_Previews: PreviewProvider {
     static var previews: some View {
